c726f632255b27957052cc1697799c27bcfaa390
FAL-coffee <fal.engineer.2001@gmail.com>
2026-02-05 00:24:13 +0900

    💡 Routeのcode baseの設定例の追加

--- frameworks/remix2/vite.config.ts
+++ frameworks/remix2/vite.config.ts
@@ -1,33 +1,35 @@
-import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@shared": path.resolve(__dirname, "../shared"),
-    },
-  },
   plugins: [
     tailwindcss(),
     remix({
       ssr: false,
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
     }),
     tsconfigPaths(),
   ],
 });
+
+// ReactRouter v7 のように、ルートをコードで定義する場合の例
+//
+// export default defineConfig({
+//   plugins: [
+//     remix({
+//       routes(defineRoutes) {
+//         return defineRoutes((route) => {
+//           route("/", "home/route.tsx", { index: true });
+//           route("about", "about/route.tsx");
+//           route("concerts", "concerts/layout.tsx", () => {
+//             route("", "concerts/home.tsx", { index: true });
+//             route("trending", "concerts/trending.tsx");
+//             route(":city", "concerts/city.tsx");
+//           });
+//         });
+//       },
+//     }),
+//   ],
+// });
